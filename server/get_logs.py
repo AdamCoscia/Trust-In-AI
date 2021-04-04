1b2f1401ee668f4c93abdd7325deea58772cf37b
Adam Coscia <47363758+AdamCoscia@users.noreply.github.com>
2021-04-04 15:15:26 -0400

    Include practice task

--- server/get_logs.py
+++ server/get_logs.py
@@ -30,15 +30,19 @@ sys.stdout.flush()
 #   if value is of type sorted sets -> ZRANGEBYSCORE <key> <min> <max>
 #
 
-users = [u.decode("utf-8") for u in r.smembers("users")]
-user_logs = {
-    u: {
-        "interactions": [json.loads(l) for l in r.lrange(f"user:{u}:interactions", 0, -1)],
-        "selections": [json.loads(l) for l in r.lrange(f"user:{u}:selections", 0, -1)],
-        "session_log": [json.loads(r.get(f"user:{u}:session"))],
-    }
-    for u in users
-}
+users = []
+user_logs = {}
+if r.smembers("users"):
+    users = [u.decode("utf-8") for u in r.smembers("users")]
+    for u in users:
+        user_logs[u] = {"interactions": None, "selections": None, "session_log": None}
+        if r.lrange(f"user:{u}:interactions", 0, -1):
+            user_logs[u]["interactions"] = [json.loads(l) for l in r.lrange(f"user:{u}:interactions", 0, -1)]
+        if r.lrange(f"user:{u}:selections", 0, -1):
+            user_logs[u]["selections"] = [json.loads(l) for l in r.lrange(f"user:{u}:selections", 0, -1)]
+        if r.get(f"user:{u}:session"):
+            user_logs[u]["session_log"] = (json.loads(r.get(f"user:{u}:session")),)
+
 
 print("=== SUMMARY ===\n")
 
@@ -52,21 +56,28 @@ else:
         if not os.path.exists(os.path.join("output", pid)):
             os.makedirs(os.path.join("output", pid))
         # Save interactions
-        df = pd.DataFrame.from_records(user_logs[pid]["interactions"])
-        df.to_csv(os.path.join("output", pid, "interactions.csv"), index=False)
-        print(df.head(), end="\n")
+        if user_logs[pid]["interactions"]:
+            df = pd.DataFrame.from_records(user_logs[pid]["interactions"])
+            df.to_csv(os.path.join("output", pid, "interactions.csv"), index=False)
+            print(df.head(), end="\n")
+            r.delete(f"user:{pid}:interactions")
+        else:
+            print("No interactions.")
         # Save selections
-        df = pd.DataFrame.from_records(user_logs[pid]["selections"])
-        df.to_csv(os.path.join("output", pid, "selections.csv"), index=False)
-        print(df.head(), end="\n")
+        if user_logs[pid]["selections"]:
+            df = pd.DataFrame.from_records(user_logs[pid]["selections"])
+            df.to_csv(os.path.join("output", pid, "selections.csv"), index=False)
+            print(df.head(), end="\n")
+            r.delete(f"user:{pid}:selections")
+        else:
+            print("No selections.")
         # Save session log
-        df = pd.DataFrame(user_logs[pid]["session_log"])
-        df.to_csv(os.path.join("output", pid, "session_log.csv"), index=False)
-        print(df.head(), end="\n")
-        # Delete keys off redis
-        r.delete(f"user:{pid}:interactions")
-        r.delete(f"user:{pid}:selections")
-        r.delete(f"user:{pid}:session")
-
-# delete users set of redis
-r.delete("users")
+        if user_logs[pid]["session_log"]:
+            df = pd.DataFrame(user_logs[pid]["session_log"])
+            df.to_csv(os.path.join("output", pid, "session_log.csv"), index=False)
+            print(df.head(), end="\n")
+            r.delete(f"user:{pid}:session")
+        else:
+            print("No session log.")
+    # delete users set of redis
+    r.delete("users")
